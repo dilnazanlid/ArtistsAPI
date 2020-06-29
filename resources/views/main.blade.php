7425a3127359a30e357e4e449ff5ef036ee343b2
Dilnaz Amanzholova <dilnaz010220@gmail.com>
2020-06-29 12:08:21 +0600

    Fixed images

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,7 +3,7 @@
 @section('title')Artists @endsection
 
 @section('content')
-  <div class="h-screen w-full" style="background-image: url('public/back.jpg'); background-size: 100% 100%;">
+  <div class="h-screen w-full" style="background-image: url('https://free4kwallpapers.com/uploads/originals/2019/07/02/road-marking-cloudy-wallpaper.jpg'); background-size: 100% 100%;">
     <div class="flex justify-center items-center">
       <a href="{{route('artists-all')}}" class="text-xl md:text-4xl font-black py-2 px-2 rounded-lg text-white pt-56 hover:text-black outline-none">See all artists</a>
     </div>
